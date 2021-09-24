2201ed8b0240c8cc4d692a6ce92fcec448aa0134
Yolfer Villalobos <yvillalobosz@gmail.com>
2021-09-24 20:49:51 -0300

    Nuevo html y css - checkout.html y checkout.css | corrección del patch de imagenes a img

--- app.js
+++ app.js
@@ -11,30 +11,35 @@ app.listen(3000, () => {
 
 //Colocamos la carpeta pblic como estática
 const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath) );
+app.use(express.static(publicPath));
 
 // ***********enlazando las páginas************************
 // Página inicio
-app.get('/',(req, res)=> {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
 // Página login
-app.get('/login',(req, res)=> {
+app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
 // Página registro de usuario
-app.get('/register',(req, res)=> {
+app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
 // Página detalle producto
-app.get('/products',(req, res)=> {
+app.get('/products', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/products.html'));
 });
 
 // carrito de compras
-app.get('/cart',(req, res)=> {
+app.get('/cart', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/cart.html'));
+});
+
+// checkout
+app.get('/checkout', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/checkout.html'));
 });
