069102346c62f343b12ea6f0262d0d142cdd1fe3
hanni0203 <hanni.file@gmail.com>
2021-11-23 02:15:36 -0500

    creación modelo User.js, mse odificó registro usuario, is ok

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require ('path');
 const { validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
+const User = require('../models/User');
 
 // convertir los datos del JSON a objeto literal
 const usersFilePath = path.join(__dirname, '../data/users.json');
@@ -16,8 +17,6 @@ const controller={
     ProcessRegister: (req, res) =>{
         const resultValidation = validationResult(req);
 
-        console.log(resultValidation.errors.length);
-
         if (resultValidation.errors.length > 0){
             return res.render('./users/register', {
                 errors: resultValidation.mapped(),
@@ -25,24 +24,32 @@ const controller={
             });
         }
 
-        
-       let newUser ={
-                cod_user: users[users.length - 1].cod_user + 1,
-                first_name: req.body.first_name,
-                last_name: req.body.last_name,
-                email: req.body.email,
-                phone: req.body.phone,
-                password: bcryptjs.hashSync(req.body.password, 10),
-                user_type: 'usuario',
-                image_profile: req.file ? req.file.filename : "user.jpg" 
-        } 
+        let userInDb = User.findByField('email', req.body.email);
 
-        users.push(newUser);
-        let userJson =JSON.stringify(users, null, " ");
-        fs.writeFileSync(usersFilePath, userJson);
-    
-        users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')); 
-        res.redirect('/') 
+        if (userInDb){
+            return res.render('./users/register',{
+                errors: {
+                    email: {
+                        msg: 'Este email ya está registrado'
+                    }
+                },
+                oldData: req.body
+            })
+        }
+
+        let userToCreate = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: bcryptjs.hashSync(req.body.password, 10),
+            user_type: 2,
+            image_profile: req.file ? req.file.filename : "user.jpg"
+        }
+
+        let userCreated = User.create(userToCreate);
+
+        res.redirect('/users/login') 
          
     },
 
---
+++ src/models/User.js
@@ -0,0 +1,62 @@
+//4. Editar la información de un usuario
+
+const fs = require('fs');
+
+const User= {
+    fileName: './src/data/users.json',
+
+    getData: function(){
+        return JSON.parse(fs.readFileSync(this.fileName, 'utf-8'));
+    },
+
+    //Genera un cod_user, teniendo en cuenta el último que está en users.json
+    generateId: function(){
+        let allUsers = this.findAll();
+        let lastUser = allUsers.pop();
+        if(lastUser){
+            return lastUser.cod_user + 1;
+        }
+        return 1;
+    },
+
+    //Trae todos los usuarios del json
+    findAll: function(){
+        return this.getData();
+    },
+
+    //Buscar a un usuario por su ID
+    findByPk: function(id){
+        let allUsers = this.findAll();
+        let userFound = allUsers.find(oneUser => oneUser.cod_user === id);
+        return userFound;
+    },
+
+    //Buscar al usuario que se quiere loguear por su email
+    findByField: function(field, text){
+        let allUsers = this.findAll();
+        let userFound = allUsers.find(oneUser => oneUser[field] === text);
+        return userFound;
+    },
+
+    //Guardar el usuario en la DB
+    create: function (userData){
+        let allUsers = this.findAll();
+        let newUser={
+            cod_user: this.generateId(),
+            ...userData
+        }
+        allUsers.push(newUser);
+        fs.writeFileSync(this.fileName, JSON.stringify(allUsers, null, ' '));
+        return true;
+    },
+
+    //Eliminar a un usuario de la DB
+    delete: function(id){
+        let allUsers = this.findAll();
+        let finalUser = allUsers.filter(oneUser => oneUser.cod_user !== id);
+        fs.writeFileSync(this.fileName, JSON.stringify(finalUser, null, ' '));
+        return true;
+    }
+}
+
+module.exports = User;
