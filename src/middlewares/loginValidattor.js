43525ee8d9627bcaa70725c2a6709229226f8203
aryelh <aryelh@gmail.com>
2021-11-25 10:28:27 -0300

    se modificaron ejs y css login campos id y value, modificacion de loginValidattor para validaciones

--- src/middlewares/loginValidattor.js
+++ src/middlewares/loginValidattor.js
@@ -4,5 +4,6 @@ const loginValidations = [
 	body('email').notEmpty().withMessage('Tienes que escribir el email de usuario'),
 	body('email').isEmail().withMessage('Escribe un email válido'),
 	body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
+	body('password').isLength({ min: 4 }).withMessage('Debe contener minimo 4 caracteres')
 ]
 module.exports = loginValidations;
